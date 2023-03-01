ce0083a796f038b8e671bae26fa2038a979d29d0
dhruvk-081 <115366526+dhruvk-081@users.noreply.github.com>
2023-03-01 21:23:09 +0000

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,90 +1,89 @@
-import React, { useState } from "react";
-import "../styles/App.css";
+import React, {Component, useState} from "react";
 
-const relation = (str1, str2) => {
-  let common = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (str2.indexOf(str1[i]) !== -1) {
-      common += str1[i];
-      str2 = str2.replace(str1[i], "");
+import '../styles/App.css';
+
+class App extends Component{
+  state = {
+    data : {name1:'',
+      name2:'',
+      answer:''}
+  }
+
+  handleChange = (e) => {
+        const data = {...this.state.data}
+        data[e.currentTarget.name] = e.currentTarget.value;
+        this.setState({data})
+  }
+
+  match = (s1,s2) => {
+      let count = 0
+      let dict = {}
+      for (let i=0;i<s1.length;i++){
+          if(s1[i] in dict){
+              dict[s1[i]]+=1
+          }else{
+              dict[s1[i]] = 1
+          }
+      }
+      for (let j=0;j<s2.length;j++){
+          if(s2[j] in dict){
+              dict[s2[j]]-=1
+              count+=1
+          }
+          if(dict[s2[j]]==0){
+              delete dict[s2[j]]
+          }
+      }
+      return count
+  }
+
+  handleClick = e => {
+    if(e.currentTarget.name == 'clear'){
+          const data = {
+              name1:'',
+              name2:'',
+              answer:''
+          }
+          this.setState({data})
+          return
+    }else if(this.state.data.name1.length && this.state.data.name2.length){
+        let count = this.state.data.name1.length+this.state.data.name2.length-(this.match(this.state.data.name1,this.state.data.name2)*2)
+        let answer;
+        if(count%6==1){
+            answer = "Friends"
+        }else if(count%6==2){
+            answer = "Love"
+        }else if(count%6==3){
+            answer = "Affection"
+        }else if(count%6==4){
+            answer = "Marriage"
+        }else if(count%6==5){
+            answer = "Enemy"
+        }else{
+            answer = "Siblings"
+        }
+        const data = {...this.state.data}
+        data.answer = answer
+        this.setState({data})
+    }else{
+        const data = {...this.state.data}
+        data.answer = 'Please Enter valid input'
+        this.setState({data})
     }
   }
 
-  let str1WithoutCommon = "";
-  for (let i = 0; i < str1.length; i++) {
-    if (common.indexOf(str1[i]) === -1) {
-      str1WithoutCommon += str1[i];
-    }
-  }
-
-  let str2WithoutCommon = "";
-  for (let i = 0; i < str2.length; i++) {
-    str2WithoutCommon += str2[i];
-  }
-
-  let totalLength = str1WithoutCommon.length + str2WithoutCommon.length;
-  switch (totalLength % 6) {
-    case 1:
-      return "Friends";
-    case 2:
-      return "Love";
-    case 3:
-      return "Affection";
-    case 4:
-      return "Marriage";
-    case 5:
-      return "Enemy";
-    case 0:
-      return "Siblings";
-    default:
-      return "";
-  }
-};
-
-const App = () => {
-  const [str1, setStr1] = useState("");
-  const [str2, setStr2] = useState("");
-  const [relationType, setRelationType] = useState("");
-
-  const handleStr1Change = (event) => {
-    setStr1(event.target.value);
-  };
-
-  const handleStr2Change = (event) => {
-    setStr2(event.target.value);
-  };
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    setRelationType(relation(str1, str2));
-  };
-
-  return (
-    <div id="main">
-      <form onSubmit={handleSubmit}>
+  render(){
+    return(
         <div>
-          <label>
-            String 1:
-            <input type="text" value={str1} onChange={handleStr1Change} />
-          </label>
+          <input data-testid="input1" value={this.state.data.name1} onChange={this.handleChange} type="text" name="name1" placeholder={'Enter first name'}/>
+          <input data-testid="input2" value={this.state.data.name2} onChange={this.handleChange} type="text" name="name2" placeholder={'Enter second name'}/>
+          <button data-testid="calculate_relationship" onClick={this.handleClick} name="calculate_relationship">Calculate Relationship Future</button>
+          <button data-testid="clear" onClick={this.handleClick} name="clear">Clear</button>
+          <h3 data-testid="answer">{this.state.data.answer}</h3>
         </div>
-        <div>
-          <label>
-            String 2:
-            <input type="text" value={str2} onChange={handleStr2Change} />
-          </label>
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-      <div>
-        {relationType === "" ? (
-          <p>Please Enter valid input</p>
-        ) : (
-          <p>Relation type: {relationType}</p>
-        )}
-      </div>
-    </div>
-  );
-};
+    )
+  }
+}
+
 
 export default App;
